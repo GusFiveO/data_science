b0ffdd08bc1cacd8f79bae895395704c943c831b
Augustin LORAIN <alorain@made-f0Cr9s4.clusters.42paris.fr>
2024-09-27 12:25:34 +0200

    switch to pgadmin4:7

--- module00/ex02/postgres/tables.sql
+++ module00/ex02/postgres/tables.sql
@@ -9,14 +9,14 @@
 -- 	user_session UUID
 -- );
 
--- CREATE TABLE IF NOT EXISTS public.data_2022_oct (
--- 	event_time timestamp,
--- 	event_type character varying(16) NOT NULL,
--- 	product_id integer NOT NULL,
--- 	price FLOAT NOT NULL,
--- 	user_id integer NOT NULL,
--- 	user_session UUID
--- );
+CREATE TABLE IF NOT EXISTS public.data_2022_oct (
+	event_time timestamp,
+	event_type character varying(16) NOT NULL,
+	product_id integer NOT NULL,
+	price FLOAT NOT NULL,
+	user_id integer NOT NULL,
+	user_session UUID
+);
 
 -- CREATE TABLE IF NOT EXISTS public.data_2022_nov (
 -- 	event_time timestamp,
@@ -27,22 +27,22 @@
 -- 	user_session UUID
 -- );
 
-CREATE TABLE IF NOT EXISTS public.data_2023_jan (
-	event_time timestamp,
-	event_type character varying(16) NOT NULL,
-	product_id integer NOT NULL,
-	price FLOAT NOT NULL,
-	user_id integer NOT NULL,
-	user_session UUID
-);
+-- CREATE TABLE IF NOT EXISTS public.data_2023_jan (
+-- 	event_time timestamp,
+-- 	event_type character varying(16) NOT NULL,
+-- 	product_id integer NOT NULL,
+-- 	price FLOAT NOT NULL,
+-- 	user_id integer NOT NULL,
+-- 	user_session UUID
+-- );
 
 -- Fill tables
 
 -- COPY public.data_2022_dec FROM '/customer/data_2022_dec.csv' DELIMITER ',' CSV HEADER;
 
--- COPY public.data_2022_oct FROM '/customer/data_2022_oct.csv' DELIMITER ',' CSV HEADER;
+COPY public.data_2022_oct FROM '/docker-entrypoint-initdb.d/customer/data_2022_oct.csv' DELIMITER ',' CSV HEADER;
 
 -- COPY public.data_2022_nov FROM '/customer/data_2022_nov.csv' DELIMITER ',' CSV HEADER;
 
-COPY public.data_2023_jan(event_time, event_type, product_id, price, user_id, user_session) FROM '/docker-entrypoint-initdb.d/customer/data_2023_jan.csv' DELIMITER ',' CSV HEADER;
+-- COPY public.data_2023_jan(event_time, event_type, product_id, price, user_id, user_session) FROM '/docker-entrypoint-initdb.d/customer/data_2023_jan.csv' DELIMITER ',' CSV HEADER;
 
