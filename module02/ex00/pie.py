7712e0ebf8f20661f565ad09f4b757bba7b96e66
Augustin LORAIN <alorain@made-f0Cr3s4.clusters.42paris.fr>
2024-10-03 11:37:00 +0200

    module02 ex01

--- module02/ex00/pie.py
+++ module02/ex00/pie.py
@@ -1,1 +1,24 @@
-import sqlalchemy
+import matplotlib.pyplot as plt
+from models import Customers
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, select, func
+
+user = "alorain"
+password = "mysecretpassword"
+host = "127.0.0.1"
+port = "5432"
+database = "piscineds"
+
+url = f"postgresql://{user}:{password}@{host}:{port}/{database}"
+
+engine = create_engine(url)
+with Session(engine) as session:
+    statement = select(Customers.event_type, func.count(Customers.event_type)).group_by(
+        Customers.event_type
+    )
+    response = session.execute(statement)
+    event_type_list = list(response)
+    event_types, count = zip(*event_type_list)
+    fig, ax = plt.subplots()
+    ax.pie(count, labels=event_types, autopct="%1.1f%%")
+    plt.show()
