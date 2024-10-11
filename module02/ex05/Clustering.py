1d3b9431d4bca74517088dd4a60d74c2b1c9167f
Augustin LORAIN <alorain@made-f0Dr8s2.clusters.42paris.fr>
2024-10-11 15:52:42 +0200

    module02 done

--- module02/ex05/Clustering.py
+++ module02/ex05/Clustering.py
@@ -17,89 +17,61 @@ database = "piscineds"
 
 url = f"postgresql://{user}:{password}@{host}:{port}/{database}"
 
-engine = create_engine(url)
-with Session(engine) as session:
 
-    statement = (
-        select(
-            Customers.user_id,
-            func.count((Customers.event_type)).label("purchase_frequency"),
-            func.sum(
-                (Customers.price),
-            ).label("total_spent"),
-        )
-        .where(Customers.event_type == "purchase")
-        .group_by(Customers.user_id)
-    )
-    response = session.execute(statement)
-    customers_features = list(response)
-    customers_features_df = pd.DataFrame(customers_features)
+def elbow_method(df, iterations):
+    within_square_errors = []
+    for nb_clusters in range(1, iterations + 1):
+        kmean = KMeans(n_clusters=nb_clusters).fit(df)
+        within_square_errors.append((nb_clusters, kmean.inertia_))
+    nb_clusters_list, ineratia_list = zip(*within_square_errors)
 
-    print(customers_features_df)
-    scaler = StandardScaler()
-    data_scaled = scaler.fit_transform(
-        customers_features_df[
-            [
-                "purchase_frequency",
-                "total_spent",
-            ]
-        ]
-    )
-    print(data_scaled)
-    wcss = []
+    plt.plot(nb_clusters_list, ineratia_list)
+    plt.ticklabel_format(style="")
+    plt.ylabel("WCSS")
+    plt.xlabel("Number of clusters")
+    plt.show()
 
-    kmeans = KMeans(
-        n_clusters=4,
-        init="k-means++",
-        max_iter=300,
-        n_init=10,
-        random_state=0,
-    )
-    customers_features_df["Cluster"] = kmeans.fit_predict(data_scaled)
 
+def preprocess_data(df, scaler, column_list):
+    data_scaled = scaler.fit_transform(df[column_list])
+    return data_scaled
+
+
+def plot_clusters(df, centroids, labels, colors):
     for cluster in customers_features_df["Cluster"].unique():
-        clustered_data = customers_features_df[
-            customers_features_df["Cluster"] == cluster
-        ]
+        clustered_data = df[df["Cluster"] == cluster]
         plt.scatter(
-            clustered_data["purchase_frequency"],
-            clustered_data["total_spent"],
-            label=f"Cluster {cluster}",
+            clustered_data["difference"],
+            clustered_data["purchase_count"],
+            label=labels[cluster],
+            color=colors[cluster],
         )
 
-    # Plot cluster centers
-    centers = scaler.inverse_transform(kmeans.cluster_centers_)
     plt.scatter(
-        centers[:, 0],
-        centers[:, 1],
+        centroids[:, 0],
+        centroids[:, 1],
         color="black",
         marker="x",
         s=100,
         label="Centroids",
     )
-
-    plt.style.use("seaborn-v0_8")
-
+    plt.ylim(bottom=0)
     plt.title("Customer Clusters by Purchase Frequency and Total Spent")
-    plt.xlabel("Purchase Frequency")
-    plt.ylabel("Total Spent")
+    plt.ylabel("number of purchase")
+    plt.xlabel("average day of purchases")
     plt.legend()
     plt.show()
 
-    cluster_counts = customers_features_df["Cluster"].value_counts()
-    print(cluster_counts)
 
-    # Plot the bar chart
-    plt.figure(figsize=(8, 5))
+def plot_cluster_distribution(df, labels, color):
 
-    cluster_labels = {1: "silver", 2: "gold", 0: "platinum", 3: "diamond"}
-    cluster_counts.index = cluster_counts.index.map(cluster_labels)
+    cluster_counts = df["Cluster"].value_counts()
+    cluster_counts.index = cluster_counts.index.map(labels)
 
-    # cluster_counts.plot(kind="bar", color=["blue", "green", "orange", "red"])
     plt.barh(
-        cluster_counts.index[::-1],
-        cluster_counts.values[::-1],
-        color=["aqua", "darkgrey", "gold", "silver"],
+        cluster_counts.index,
+        cluster_counts.values,
+        color=color,
     )
 
     plt.title("Customers in Each Cluster")
@@ -111,3 +83,71 @@ with Session(engine) as session:
     plt.gca().xaxis.set_major_formatter(formatter)
 
     plt.show()
+
+
+engine = create_engine(url)
+with Session(engine) as session:
+
+    cte = select(
+        func.max(func.date_trunc("day", Customers.event_time)).label("latest_day")
+    ).cte("max_day")
+
+    statement = (
+        select(
+            Customers.user_id,
+            func.count(Customers.event_type).label("purchase_count"),
+            func.avg(
+                func.extract(
+                    "day",
+                    cte.c.latest_day - func.date_trunc("day", Customers.event_time),
+                )
+            ).label("difference"),
+        )
+        .where(Customers.event_type == "purchase")
+        .group_by(Customers.user_id, cte.columns.latest_day)
+        .order_by(Customers.user_id, "difference")
+    )
+    print(statement)
+
+    response = session.execute(statement)
+    customers_features = list(response)
+    customers_features_df = pd.DataFrame(customers_features)
+
+    scaler = StandardScaler()
+
+    data_scaled = preprocess_data(
+        customers_features_df, scaler, ["difference", "purchase_count"]
+    )
+
+    plt.style.use("seaborn-v0_8")
+    elbow_method(data_scaled, 10)
+
+    kmeans = KMeans(n_clusters=3, random_state=42)
+    customers_features_df["Cluster"] = kmeans.fit_predict(data_scaled)
+    centroids = scaler.inverse_transform(kmeans.cluster_centers_)
+    colors = ["grey", "green", "cyan"]
+    cluster_labels = {0: "inactive", 1: "new customers", 2: "loyal_customers"}
+    plot_clusters(customers_features_df, centroids, cluster_labels, colors)
+
+    plot_cluster_distribution(customers_features_df, cluster_labels, colors)
+    cluster_counts = customers_features_df["Cluster"].value_counts()
+    loyal_customers_idx = cluster_counts.idxmin()
+
+    loyal_customers_cluster = customers_features_df[
+        customers_features_df["Cluster"] == loyal_customers_idx
+    ]
+
+    data_scaled = preprocess_data(
+        loyal_customers_cluster, scaler, ["difference", "purchase_count"]
+    )
+
+    elbow_method(data_scaled, 10)
+
+    kmeans = KMeans(n_clusters=3, random_state=42)
+    loyal_customers_cluster.loc[:, "Cluster"] = kmeans.fit_predict(data_scaled)
+    centroids = scaler.inverse_transform(kmeans.cluster_centers_)
+    colors = ["silver", "gold", "whitesmoke"]
+    cluster_labels = {0: "silver", 1: "gold", 2: "platinum"}
+    plot_clusters(loyal_customers_cluster, centroids, cluster_labels, colors)
+
+    plot_cluster_distribution(loyal_customers_cluster, cluster_labels, colors)
