306b716876e0a1f25df03ae72d29f8ccd8264b63
Augustin LORAIN <alorain@made-f0Br3s11.clusters.42paris.fr>
2024-10-15 15:27:22 +0200

    module03/ex05

--- module03/ex05/split.py
+++ module03/ex05/split.py
@@ -1,15 +1,20 @@
 import pandas as pd
+import sys
 
-train_file_path = "../Train_knight.csv"
+# train_file_path = "../Train_knight.csv"
+try:
+    train_file_path = sys.argv[1]
 
-train_knights_df = pd.read_csv(train_file_path)
+    train_knights_df = pd.read_csv(train_file_path)
 
-shuffled_df = train_knights_df.sample(frac=1)
+    shuffled_df = train_knights_df.sample(frac=1)
 
-split_index = round(0.80 * len(shuffled_df))
+    split_index = round(0.80 * len(shuffled_df))
 
-train_df = shuffled_df.iloc[:split_index, :]
-validation_df = shuffled_df.iloc[split_index:, :]
+    train_df = shuffled_df.iloc[:split_index, :]
+    validation_df = shuffled_df.iloc[split_index:, :]
 
-train_df.to_csv("Training_knight.csv")
-validation_df.to_csv("Validation_knight.csv")
+    train_df.to_csv("Training_knight.csv")
+    validation_df.to_csv("Validation_knight.csv")
+except Exception as e:
+    print(e)
