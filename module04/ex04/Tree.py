dcecaf82c89178f29b43d9bb3abe2172d0786a0d
Augustin LORAIN <alorain@paul-f4Br5s8.clusters.42paris.fr>
2024-10-21 14:18:52 +0200

    change args in module04/ex04

--- module04/ex04/Tree.py
+++ module04/ex04/Tree.py
@@ -50,42 +50,49 @@ def feature_selection(df):
 
 
 try:
-    training_file_path = sys.argv[1]
-    validation_file_path = sys.argv[2]
+    train_file_path = sys.argv[1]
+    test_file_path = sys.argv[2]
 
-    training_df = pd.read_csv(training_file_path)
-    validation_df = pd.read_csv(validation_file_path)
+    train_df = pd.read_csv(train_file_path)
+    test_df = pd.read_csv(test_file_path)
 
-    target = training_df["knight"]
-    training_df = training_df.drop("knight", axis=1)
+    validation_df = train_df.sample(frac=0.2, random_state=42)
+    train_df = train_df.drop(validation_df.index)
+
+    truth = validation_df["knight"]
+    target = train_df["knight"]
+    train_df = train_df.drop("knight", axis=1)
 
     truth = validation_df["knight"]
     validation_df = validation_df.drop("knight", axis=1)
 
     validation_df = feature_selection(validation_df)
-    training_df = feature_selection(training_df)
+    train_df = feature_selection(train_df)
+    test_df = feature_selection(test_df)
 
     scaler = StandardScaler()
-    scaled_values = scaler.fit_transform(training_df)
-    training_df.loc[:, :] = scaled_values
+    scaled_values = scaler.fit_transform(train_df)
+    train_df.loc[:, :] = scaled_values
 
     scaled_values = scaler.fit_transform(validation_df)
     validation_df.loc[:, :] = scaled_values
 
     clf = RandomForestClassifier(class_weight="balanced")
-    clf.fit(training_df, target)
+    clf.fit(train_df, target)
 
     prediction = clf.predict(validation_df)
     print(
         "F1-Score:",
         f1_score(truth, prediction, labels=["Jedi", "Sith"], average="weighted"),
     )
-    save_list("Tree.txt", prediction)
+
+    test_prediction = clf.predict(test_df)
+    save_list("Tree.txt", test_prediction)
 
     fig, axes = plt.subplots(dpi=900)
     tree.plot_tree(
         clf.estimators_[0],
-        feature_names=training_df.columns,
+        feature_names=train_df.columns,
         class_names=["Jedi", "Sith"],
         filled=True,
     )
